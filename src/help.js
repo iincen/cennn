7b8936afc45ba37a91694d1793ae3808e4bf7d33
`c3n` <73513832+iincen@users.noreply.github.com>
2021-09-26 18:21:55 +0700

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,79 +1,90 @@
 const help = (prefix, copid, tanggal, jams, botname) => {
-	return `
-*_- - - - - [ BOT CEN ] - - - - -_*
+	return `「  *${botname}*  」
+┌━━━━━━━━━━━━┈ ❋ཻུ۪۪⸙
+│ *⦿ Bot Prefix :* [all prefix]
+│ *⦿ Owner :* FebzGanz
+│ *⦿ Today :* ${tanggal}
+│ *⦿ Jam :* ${jams}
+│ *⦿ Ig:* https://bit.ly/3hqqQzZ
+└┬────────────┈ ⳹
+┌┤ 「  *Kasus Covid-19 Indonesia*  」
+││➥ *Terinfeksi :* ${copid[0].kasus}
+││➥ *Kematian :* ${copid[0].kematian}
+││➥ *Sembuh :* ${copid[0].sembuh}
+│└────────────┈ ⳹
 
 + _no spam, no telp, no vc_
 + _melanggar rul ? blok_
 
 _*[+] - [ Grup Menu ]*_
 
-. _!setname_ <text>
-. _!setdesc_ <text>
-. _!setpp_ <img>
-. _!promote_ <tag>
-. _!demote_ <tag>
-. _!leave_
-. _!tagall_ 
-. _!hidetag_	
-. _!welcome_ <1/0>
-. _!listadmin_
-. _!antilink_ <1/0>
-. _!add_ <62×××>
-. _!kick_ <tag mem>
-. _!group_ buka/tutup
-. _!linkgc_
+. _*${prefix}setname_ <text>
+. _*${prefix}setdesc_ <text>
+. _*${prefix}setpp_ <img>
+. _*${prefix}promote_ <tag>
+. _*${prefix}demote_ <tag>
+. _*${prefix}leave_
+. _*${prefix}tagall_ 
+. _*${prefix}hidetag_	
+. _*${prefix}welcome_ <1/0>
+. _*${prefix}listadmin_
+. _*${prefix}antilink_ <1/0>
+. _*${prefix}add_ <62×××>
+. _*${prefix}kick_ <tag mem>
+. _*${prefix}group_ buka/tutup
+. _*${prefix}linkgc_
 
 _*[+] - [ Maker ]*_
 
-. _!tomp3_
-. _!lirik_
-. _!sticker_
-. _!swm_ <author>|<pack>
-. _!tovideo_
-. _!toimg_
-. _!tts_ <Text>
-. _!nulis_ <Text>
+. _*${prefix}tomp3_
+. _*${prefix}lirik_
+. _*${prefix}sticker_
+. _*${prefix}swm_ <author>|<pack>
+. _*${prefix}tovideo_
+. _*${prefix}toimg_
+. _*${prefix}tts_ <Text>
+. _*${prefix}nulis_ <Text>
 
 _*[+] - [ Downloader ]*_
 
-. _!ytmp3_ <Url>
-. _!ytmp4_ <Url>
-. _!dafontdown_ <Url>
-. _!facebook_ <Url>
-. _!instagram_ <Url> <Options>
-. _!tiktok_ <Url>
-. _!soundcloud_ <Url>
-. _!pinterest_ <Query>
-. _!play_ <Query> 
-. _!joox_ <Query>
+. _*${prefix}ytmp3_ <Url>
+. _*${prefix}ytmp4_ <Url>
+. _*${prefix}dafontdown_ <Url>
+. _*${prefix}facebook_ <Url>
+. _*${prefix}instagram_ <Url> <Options>
+. _*${prefix}tiktok_ <Url>
+. _*${prefix}soundcloud_ <Url>
+. _*${prefix}pinterest_ <Query>
+. _*${prefix}play_ <Query> 
+. _*${prefix}joox_ <Query>
 
 _*[+] - [ Search ]*_
 
-. _!ytsearch_ <Query>
-. _!ytdesc_ <Query>
-. _!brainly_ <Query>
-. _!herolist_
-. _!herodetail_ <Nama hero>
-. _!dafontsearch_ <Query>
-. _!google_ <Query>
-. _!wiki_ <Query>
-. _!quotes_ <Query>
+. _*${prefix}ytsearch_ <Query>
+. _*${prefix}ytdesc_ <Query>
+. _*${prefix}brainly_ <Query>
+. _*${prefix}herolist_
+. _*${prefix}herodetail_ <Nama hero>
+. _*${prefix}dafontsearch_ <Query>
+. _*${prefix}google_ <Query>
+. _*${prefix}wiki_ <Query>
+. _*${prefix}quotes_ <Query>
 
 _*[+] - [ Nolep Menu ]*_
 
-. _!whatanime_ <FotoAnim>
-. _!animelist_
-. _!randomhentong_
+. _*${prefix}whatanime_ <FotoAnim>
+. _*${prefix}animelist_
+. _*${prefix}randomhentong_
 
 _*[+] - [ Info Command ]*_
 
 Tanda Kurung < > Hiraukan Contoh :
-_!play fly away_
+_*${prefix}play fly away_
 
-``Lapor Bug🐞: _!bugreport_
-``Request?    : _!request_
-``owner?      : _!owner_
-``donate?     : _!donasi_
+``Lapor Bug🐞: _*${prefix}bugreport_
+``Request?    : _*${prefix}request_
+``owner?      : _*${prefix}owner_
+``donate?     : _*${prefix}donasi_
 
 Have a Nice Day ❋ཻུ۪۪⸙`}
 
