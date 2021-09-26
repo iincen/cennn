905b732229689d230f5c5f338ff804a34734fd3c
`c3n` <73513832+iincen@users.noreply.github.com>
2021-09-26 18:58:48 +0700

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,77 +1,79 @@
 const help = (prefix, botname) => {
 	return `「  *${botname}*  」
-+ *no spam, no telp, no vc*
-+ *melanggar rul ? blok*
-
-**[+] - [ Grup Menu ]**
-
- *${prefix}setname* <text>
- *${prefix}setdesc* <text>
- *${prefix}setpp* <img>
- *${prefix}promote* <tag>
- *${prefix}demote* <tag>
- *${prefix}leave*
- *${prefix}tagall* 
- *${prefix}hidetag*	
- *${prefix}welcome* <1/0>
- *${prefix}listadmin*
- *${prefix}antilink* <1/0>
- *${prefix}add* <62×××>
- *${prefix}kick* <tag mem>
- *${prefix}group* buka/tutup
- *${prefix}linkgc*
-
-**[+] - [ Maker ]**
-
- *${prefix}tomp3*
- *${prefix}lirik*
- *${prefix}sticker*
- *${prefix}swm* <author>|<pack>
- *${prefix}tovideo*
- *${prefix}toimg*
- *${prefix}tts* <Text>
- *${prefix}nulis* <Text>
-
-**[+] - [ Downloader ]**
-
- *${prefix}ytmp3* <Url>
- *${prefix}ytmp4* <Url>
- *${prefix}dafontdown* <Url>
- *${prefix}facebook* <Url>
- *${prefix}instagram* <Url> <Options>
- *${prefix}tiktok* <Url>
- *${prefix}soundcloud* <Url>
- *${prefix}pinterest* <Query>
- *${prefix}play* <Query> 
- *${prefix}joox* <Query>
-
-**[+] - [ Search ]**
-
- *${prefix}ytsearch* <Query>
- *${prefix}ytdesc* <Query>
- *${prefix}brainly* <Query>
- *${prefix}herolist*
- *${prefix}herodetail* <Nama hero>
- *${prefix}dafontsearch* <Query>
- *${prefix}google* <Query>
- *${prefix}wiki* <Query>
- *${prefix}quotes* <Query>
-
-**[+] - [ Nolep Menu ]**
-
- *${prefix}whatanime* <FotoAnim>
- *${prefix}animelist*
- *${prefix}randomhentong*
-
-**[+] - [ Info Command ]**
-
-Tanda Kurung < > Hiraukan Contoh :
-*${prefix}play fly away*
-
-``Lapor Bug🐞: *${prefix}bugreport*
-``Request?    : *${prefix}request*
-``owner?      : *${prefix}owner*
-
-Have a Nice Day ❋ཻུ۪۪⸙`}
+┌━━━━━━━━━━━━┈ ❋ཻུ۪۪⸙
+│ 「 *Group Menu* 」
+│◦ *${prefix}setname* [text]
+│◦ *${prefix}setdesc* [text]
+│◦ *${prefix}setpp* [img]
+│◦ *${prefix}promote* [tag]
+│◦ *${prefix}demote* [tag]
+│◦ *${prefix}leave*
+│◦ *${prefix}tagall* 
+│◦ *${prefix}hidetag*
+│◦ *${prefix}welcome* [1/0]
+│◦ *${prefix}listadmin*
+│◦ *${prefix}antilink* [1/0]
+│◦ *${prefix}add* [62×××]
+│◦ *${prefix}kick* [tag mem]
+│◦ *${prefix}group* buka/tutup
+│◦ *${prefix}linkgc*
+├─────────────┈ ⳹
+│ 「 *Maker* 」
+│◦ *${prefix}tomp3*
+│◦ *${prefix}lirik*
+│◦ *${prefix}tovn*
+│◦ *${prefix}sticker*
+│◦ *${prefix}swm* [author]|[pack]
+│◦ *${prefix}tovideo*
+│◦ *${prefix}toimg*
+│◦ *${prefix}tts* [Text]
+│◦ *${prefix}nulis* [Text]
+├─────────────┈ ⳹
+│ 「 *Downloader* 」
+│◦ *${prefix}ytmp3* [Url]
+│◦ *${prefix}ytmp4* [Url]
+│◦ *${prefix}dafontdown* [Url]
+│◦ *${prefix}facebook* [Url]
+│◦ *${prefix}instagram* [Url] [Options]
+│◦ *${prefix}tiktok* [Url]
+│◦ *${prefix}soundcloud* [Url]
+│◦ *${prefix}pinterest* [Query]
+│◦ *${prefix}play* [Query] [Options]
+│◦ *${prefix}joox* [Query]
+├─────────────┈ ⳹
+│ 「 *Searching* 」
+│◦ *${prefix}ytsearch* [Query]
+│◦ *${prefix}ytdesc* [Query]
+│◦ *${prefix}brainly* [Query]
+│◦ *${prefix}herolist*
+│◦ *${prefix}herodetail* [Nama hero]
+│◦ *${prefix}dafontsearch* [Query]
+│◦ *${prefix}google* [Query]
+│◦ *${prefix}wiki* [Query]
+│◦ *${prefix}quotes* [Query]
+│◦ *${prefix}preview* teks|ukuran 
+│◦ *${prefix}ocr*
+├─────────────┈ ⳹
+│ 「 *New Fiture* 」
+│◦ *${prefix}clearall*
+│◦ *${prefix}bc* [Text]
+│◦ *${prefix}preview* [Text]
+│◦ *next update sc w add new fitur*
+├─────────────┈ ⳹
+│ 「 *Wibu Menu* 」
+│◦ *${prefix}whatanime* [FotoAnim]
+│◦ *${prefix}animelist*
+│◦ *${prefix}randomhentong*
+│◦ *next update sc w add new fitur*
+├─────────────┈ ⳹
+││Tanda Kurung [ ] Hiraukan Contoh :
+││ *${prefix}play fly away*
+││
+││Lapor Bug🐞: *${prefix}bugreport*
+││Request? : *${prefix}request*
+││owner? : *${prefix}owner*
+│└────────────┈ ⳹
+│ *Have A Nice Day*
+└━━━━━━━━━━━━┈ ❋ཻུ۪۪⸙`}
 
 exports.help = help
