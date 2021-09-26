d5dcb7c672609d8fd11d5308f4c6dd5adbacc5cd
CEN <vincentiusanto36@gmail.com>
2021-09-26 16:27:41 +0700

    Menambahkan file commit

---
+++ plugins/donasi.js
@@ -0,0 +1,20 @@
+const donasi = () => { 
+	return `       
+Hello, terimakasih telah menggunakan bot ini, untuk mendukung bot ini kamu dapat membantu dengan berdonasi dengan cara:
+
+LinkAja : 085156760338
+Pulsa   : 085156760338
+Saweria : https://saweria.co/incen
+
+Doakan agar project bot ini bisa terus berkembang
+Doakan agar author bot ini dapat ide-ide yang kreatif lagi
+
+Donasi akan digunakan untuk pengembangan dan pengoperasian bot ini.
+
+semoga harimu menyenangkan :)
+
+Terimakasih.
+`
+}
+
+exports.donasi = donasi
---
+++ src/help.js
@@ -0,0 +1,89 @@
+const help = (prefix, copid, tanggal, jams, botname) => {
+	return `「  *${botname}*  」
+┌━━━━━━━━━━━━┈ ❋ཻུ۪۪⸙
+│ *⦿ Bot Prefix :* [all prefix]
+│ *⦿ Owner :* Canz
+│ *⦿ Today :* ${tanggal}
+│ *⦿ Jam :* ${jams}
+└┬────────────┈ ⳹
+┌┤ 「  *Kasus Covid-19 Indonesia*  」
+││➥ *Terinfeksi :* ${copid[0].kasus}
+││➥ *Kematian :* ${copid[0].kematian}
+││➥ *Sembuh :* ${copid[0].sembuh}
+│└────────────┈ ⳹
+│ 「 *Group Menu* 」
+│◦➛ *${prefix}setname* [text]
+│◦➛ *${prefix}setdesc* [text]
+│◦➛ *${prefix}setpp* [img]
+│◦➛ *${prefix}promote* [tag]
+│◦➛ *${prefix}demote* [tag]
+│◦➛ *${prefix}leave*
+│◦➛ *${prefix}tagall* 
+│◦➛ *${prefix}hidetag*
+│◦➛ *${prefix}welcome* [1/0]
+│◦➛ *${prefix}listadmin*
+│◦➛ *${prefix}antilink* [1/0]
+│◦➛ *${prefix}add* [62×××]
+│◦➛ *${prefix}kick* [tag mem]
+│◦➛ *${prefix}group* buka/tutup
+│◦➛ *${prefix}linkgc*
+├─────────────┈ ⳹
+│ 「 *Maker* 」
+│◦➛ *${prefix}tomp3*
+│◦➛ *${prefix}lirik*
+│◦➛ *${prefix}tovn*
+│◦➛ *${prefix}sticker*
+│◦➛ *${prefix}swm* [author]|[pack]
+│◦➛ *${prefix}tovideo*
+│◦➛ *${prefix}toimg*
+│◦➛ *${prefix}tts* [Text]
+│◦➛ *${prefix}nulis* [Text]
+├─────────────┈ ⳹
+│ 「 *Downloader* 」
+│◦➛ *${prefix}ytmp3* [Url]
+│◦➛ *${prefix}ytmp4* [Url]
+│◦➛ *${prefix}dafontdown* [Url]
+│◦➛ *${prefix}facebook* [Url]
+│◦➛ *${prefix}instagram* [Url] [Options]
+│◦➛ *${prefix}tiktok* [Url]
+│◦➛ *${prefix}soundcloud* [Url]
+│◦➛ *${prefix}pinterest* [Query]
+│◦➛ *${prefix}play* [Query] [Options]
+│◦➛ *${prefix}joox* [Query]
+├─────────────┈ ⳹
+│ 「 *Searching* 」
+│◦➛ *${prefix}ytsearch* [Query]
+│◦➛ *${prefix}ytdesc* [Query]
+│◦➛ *${prefix}brainly* [Query]
+│◦➛ *${prefix}herolist*
+│◦➛ *${prefix}herodetail* [Nama hero]
+│◦➛ *${prefix}dafontsearch* [Query]
+│◦➛ *${prefix}google* [Query]
+│◦➛ *${prefix}wiki* [Query]
+│◦➛ *${prefix}quotes* [Query]
+│◦➛ *${prefix}preview* teks|ukuran 
+│◦➛ *${prefix}ocr*
+├─────────────┈ ⳹
+│ 「 *New Fiture* 」
+│◦➛ *${prefix}clearall*
+│◦➛ *${prefix}bc* [Text]
+│◦➛ *${prefix}preview* [Text]
+│◦➛ *next update sc w add new fitur*
+├─────────────┈ ⳹
+│ 「 *Wibu Menu* 」
+│◦➛ *${prefix}whatanime* [FotoAnim]
+│◦➛ *${prefix}animelist*
+│◦➛ *${prefix}randomhentong*
+│◦➛ *next update sc w add new fitur*
+├─────────────┈ ⳹
+││Tanda Kurung [ ] Hiraukan Contoh :
+││ *${prefix}play fly away*
+││
+││Lapor Bug🐞: *${prefix}bugreport*
+││Request? : *${prefix}request*
+││owner? : *${prefix}owner*
+│└────────────┈ ⳹
+│ *Have A Nice Day*
+└━━━━━━━━━━━━┈ ❋ཻུ۪۪⸙`}
+
+exports.help = help
